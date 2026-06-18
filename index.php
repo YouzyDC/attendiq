3d56d9810c70956120c61bc7019692a9be9f4195
YouzyDC <166558737+YouzyDC@users.noreply.github.com>
2026-06-18 17:14:44 +0100

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/include/config.php';
-if (isLoggedIn()) {
-    header('Location: ' . SITE_URL . '/admin/dashboard.php');
-} else {
-    header('Location: ' . SITE_URL . '/admin/login.php');
-}
-exit;
